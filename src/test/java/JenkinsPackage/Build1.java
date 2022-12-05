bc8e6c980bd6a622978022a224db53809e260ec5
vigne_txpavsv <vigne_txpavsv@DESKTOP-QLSIQ3F>
2022-12-05 16:29:47 +0530

    done

---
+++ src/test/java/JenkinsPackage/Build1.java
@@ -0,0 +1,12 @@
+package JenkinsPackage;
+
+import org.testng.annotations.Test;
+
+public class Build1 {
+	@Test
+	public void BuildNo1(){
+		System.out.println("build1");
+	
+
+}
+}
---
+++ src/test/java/JenkinsPackage/Build3.java
@@ -0,0 +1,16 @@
+package JenkinsPackage;
+
+import org.testng.annotations.Test;
+
+public class Build3 {
+	
+	
+		@Test
+		public void BuildNo1(){
+			System.out.println("build1");
+		
+
+	}
+	}
+
+
